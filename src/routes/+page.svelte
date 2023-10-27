2ce7afdafc7ae60b932cd6fb225000065769224d
rouanw <rouanw@users.noreply.github.com>
2023-10-27 16:34:47 +0100

    Content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero">
+    <a href="https://www.edinburghzoo.org.uk/animals-and-experiences/animals/visayan-warty-pig/">
+        <img
+                class="hero-image"
+                alt="Warty Piglet"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/WartyPiglet.JPG/512px-WartyPiglet.JPG"
+        /></a
+    >
+    <h1>Warty Pig</h1>
+    <div class="def-wrapper">
+        <span class="def-intro">1</span>
+        <span class="def-content"
+        >The Visayan warty pig <i>(Sus cebifrons)</i> is a critically endangered species in the pig genus
+			endemic to six of the Visayan Islands</span
+        >
+    </div>
+    <div>
+        <span class="def-intro">2</span>
+        <span class="def-content"
+        >A small consultancy, dedicated to delivering high-quality solutions to our clients
+		</span>
+    </div>
+</div>
+<div class="services">
+    <span>💻 Software Engineering</span>
+    <span>🧭 Leadership</span>
+    <span>✏️ Copywriting</span>
+    <span>📝 Editing</span>
+</div>
+<div>
+    <a class="link" href="https://forms.gle/w4VsVhBkHfvjT26y6" target="_blank" rel="noreferrer"
+    >Contact us 💬</a
+    >
+</div>
+
+<style>
+    h1 {
+        font-family: 'Young Serif', 'Georgia', serif;
+    }
+
+    .def-intro {
+        padding: 0.2rem;
+    }
+
+    .def-content {
+        font-size: 1.2rem;
+        line-height: 2rem;
+    }
+
+    .def-wrapper {
+        line-height: 1.8rem;
+    }
+
+    img {
+        max-width: 100%;
+        display: block;
+    }
+
+    .hero {
+        border-bottom: 2pt solid #91856d;
+        padding: 5rem 2rem;
+    }
+
+    .hero-image {
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    }
+
+    .services {
+        padding: 2rem;
+        display: flex;
+        flex-wrap: wrap;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 1rem;
+        font-size: 1.2rem;
+        line-height: 1.8rem;
+    }
+
+    .link {
+        display: inline-block;
+        padding: 0.8rem 1.6rem;
+        color: #4a7cdb;
+        text-decoration: none;
+        border-radius: 4px;
+        font-size: 1.2em;
+    }
+
+    .link:hover {
+        color: #2662d5;
+    }
+</style>
